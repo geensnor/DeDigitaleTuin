8083fa57e169a5c99352058bd4c256e9a2f0f7d5
Joris <jorisreijbroek@gmail.com>
2026-02-25 13:04:06 +0100

    dupe keys zijn prima

--- eslint.config.js
+++ eslint.config.js
@@ -11,6 +11,9 @@ export default defineConfig([
     plugins: { js },
     extends: ["js/recommended"],
     languageOptions: { globals: globals.browser },
+    rules: {
+      "no-dupe-keys": "off",
+    },
   },
   ...tseslint.configs.recommended,
   {
